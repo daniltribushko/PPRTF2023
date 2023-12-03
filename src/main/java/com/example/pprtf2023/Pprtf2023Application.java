1390b093d61725b5a9e026e0063fecaa85a3ec82
daniltribushko <105868780+daniltribushko@users.noreply.github.com>
2023-12-03 14:47:48 +0500

    initial commit

---
+++ src/main/java/com/example/pprtf2023/Pprtf2023Application.java
@@ -0,0 +1,13 @@
+package com.example.pprtf2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pprtf2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pprtf2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pprtf2023/Pprtf2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pprtf2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pprtf2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
